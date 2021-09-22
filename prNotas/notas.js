4e53cacd579a680e7020c5609bd56573c62d19ac
hermo18 <79795566+hermo18@users.noreply.github.com>
2021-09-22 20:21:02 +0200

    prNotas
    calcula nota en texto

---
+++ prNotas/notas.js
@@ -0,0 +1,18 @@
+let n=prompt("INTODUCE UNA NOTA: ");
+if (n<5) {
+    console.log("INSUFICIENTE");}
+if (n>=5 && n<6) {
+    console.log("SUFICIENTE");
+}
+if (n>=6 && n<7) {
+    console.log("BIEN");
+}
+if (n>=7 && n<9) {
+    console.log("NOTABLE");
+}
+if (n>=9 && n<10) {
+    console.log("SOBRESALIENTE");
+}
+if (n==10) {
+    console.log("EXCELENTE");
+}
