660a514b112140d07784f87eafdb17cfcd042531
hermo18 <79795566+hermo18@users.noreply.github.com>
2021-10-06 18:33:41 +0200

    pr4-5 acabado

--- pr4-5/contraseña.js
+++ pr4-5/contraseña.js
@@ -1,29 +1,74 @@
+let minusculas="abcdefghyjklmnñopqrstuvwxyz";
+let mayusculas=minusculas.toUpperCase();
+let caracteres="*-_;";
+let numeros="0123456789";
+
+let contadorM=0;
+let contadorC=0;
+let nombreBien=false;
+let passBien=false;
+
+let min=0;
+let may=0;
+let car=0;
+let num=0;
+
 let nombre=prompt("INTRODUZCA SU NOMBRE DE USUARIO: ");
-let pass=prompt("INTRODUZCA SU CONTRASEÑA: ");
 
-let control=true;
 
-while (nombre!=null && control) {
+while (nombre!==null && !nombreBien) {
+    contadorM=0;
+    contadorC=0;
     for (let i = 0; i < nombre.length; i++) {
-        if((nombre.charAt(i)<'a' || nombre.charAt(i)>'z') && (nombre.charAt(i)<0 || nombre.charAt(i)>9)){
-            control=false;
+        if (mayusculas.includes(nombre.charAt(i))==true) {
+            contadorM++;
+        }
+        if (caracteres.includes(nombre.charAt(i))==true) {
+            contadorC++;
         }
     }
-    if (control==false) {
-        control=true;
-        console.log("NOMBRE INCORRECTO");
+    if (contadorM>0 ||contadorC>0) {
+        console.log("NOMBRE ERRONEO");
         nombre=prompt("INTRODUZCA SU NOMBRE DE USUARIO: ");
     }else{
-        break;
+        console.log("NOMBRE CORRECTO");
+        nombreBien=true;
     }
 }
-console.log("OLE");
-
-
-
-
-
 
 if (nombre===null) {
     console.log("CANCELADO");
+}else{
+    let contrasenya=prompt("INTRODUZCA SU CONTRASEÑA: ");
+    if (contrasenya===null) {
+        console.log("CANCELADO");
+    }
+    while (contrasenya!==null && !passBien) {
+        min=0;
+        may=0;
+        car=0;
+        num=0;
+        for (let i = 0; i < contrasenya.length; i++) {
+            if (mayusculas.includes(contrasenya.charAt(i))==true) {
+                may++;
+            }
+            if (minusculas.includes(contrasenya.charAt(i))==true) {
+                min++;
+            }
+            if (caracteres.includes(contrasenya.charAt(i))==true) {
+                car++;
+            }
+            if (numeros.includes(contrasenya.charAt(i))==true) {
+                num++;
+            }
+        }
+        if (may<1||min<1||car<1||num<1) {
+            console.log("CONTRASEÑA ERRONEO");
+            contrasenya=prompt("INTRODUZCA SU CONTRSEÑA: ");
+        }else{
+            console.log("CONTRASEÑA CORRECTA");
+            passBien=true;
+        }
+    } 
 }
+
