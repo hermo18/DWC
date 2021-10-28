95670c4e806fdb596b90eebb9a04ce78658f7eee
hermo18 <79795566+hermo18@users.noreply.github.com>
2021-10-28 19:17:20 +0200

    Update cartas.js

--- cartas clases/cartas.js
+++ cartas clases/cartas.js
@@ -6,10 +6,28 @@ class Carta{
 }
 
 let baraja=[];
+let carta;
 
 for (let i = 0; i < 4; i++) {
     for (let j = 1; j < 13; j++) {
-        let carta=new Carta(i, j);
+        switch (i) {
+            case 0:
+                carta=new Carta("oro", j);
+                break;
+            case 1:
+                carta=new Carta("copas", j);
+                break;
+            case 2:
+                carta=new Carta("bastos", j);
+                break;
+            case 3:
+                carta=new Carta("espadas", j);
+                break;
+        
+            default:
+                break;
+        }
+        
         baraja.push(carta);
     } 
 }
@@ -25,23 +43,29 @@ function aleatorio(min, max) {
 
 
 let eleccion=prompt("SELECCIONA 1 PARA BARAJAR, 2 PARA SACAR CARTA, 3 PARA MOSTRAR BARAJA Y 4 PARA SALIR:");
-
-while(eleccion!="4"){
-    switch (eleccion) {
-        case "1":
-            barajar(baraja);
-            console.log("BARAJADO");
-            break;
-        case "2":
-            console.log(baraja[aleatorio(0, baraja.length)]);
-            break;
-        case "3":
-            console.log(baraja);
-            break;
-        default:
-            console.log("OPCIÓN NO VÁLIDA");
-            break;
+if (eleccion!=null) {
+    while(eleccion!="4"){
+        switch (eleccion) {
+            case "1":
+                barajar(baraja);
+                console.log("BARAJADO");
+                break;
+            case "2":
+                console.log(baraja[aleatorio(0, baraja.length)]);
+                break;
+            case "3":
+                for (let i = 0; i < baraja.length; i++) {
+                    console.log(baraja[i]);  
+                }
+                break;
+            default:
+                console.log("OPCIÓN NO VÁLIDA");
+                break;
+        }
+        eleccion=prompt("SELECCIONA 1 PARA BARAJAR, 2 PARA SACAR CARTA, 3 PARA MOSTRAR BARAJA Y 4 PARA SALIR:");
     }
-    eleccion=prompt("SELECCIONA 1 PARA BARAJAR, 2 PARA SACAR CARTA, 3 PARA MOSTRAR BARAJA Y 4 PARA SALIR:");
+}
+if (eleccion==null) {
+    console.log("CANCELADO");
 }
 console.log("ADIÓS");
